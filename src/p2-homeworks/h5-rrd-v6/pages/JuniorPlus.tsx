24ea7d3ebe40c29bf26c301ed9e40e7818ee32e3
Kirill <apatski94@gmail.com>
2022-10-29 21:54:17 +0300

    hw 11 and refactoring files (loadingReducer, Pages, store)

--- src/p2-homeworks/h5-rrd-v6/pages/JuniorPlus.tsx
+++ src/p2-homeworks/h5-rrd-v6/pages/JuniorPlus.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import HW11 from "../../h11/HW11";
 
 export const JuniorPlus = () => {
     return (
         <div>
-            Junior-plus
+            <HW11/>
         </div>
     );
 };
